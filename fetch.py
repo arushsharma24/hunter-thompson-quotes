ee270ff46d2466b4c6bc54bc077517a00e4f750b
Arush Sharma <arush24astar@gmail.com>
2023-04-28 06:35:00 +0530

    works perfectly as far as I can see now, stores quotes as a list of json objects in a json file quotes.json

--- fetch.py
+++ fetch.py
@@ -2,12 +2,10 @@ import json  # to make this into a json file
 import requests
 from bs4 import BeautifulSoup as bs4
 
-# Clear the json file first
-with open("quotes.json", "w") as outfile:
-    pass  # Do nothing - this will clear the file
-
 quotes = []
 
+x = 1
+fresh = True
 for page in range(1, 30):
     url = "https://www.goodreads.com/author/quotes/5237.Hunter_S_Thompson?page=" + \
         str(page)
@@ -18,14 +16,30 @@ for page in range(1, 30):
     quote_divs = soup.find_all("div", {"class": "quoteText"})
 
     for div in quote_divs:
-        # for some reason this weird dash which I can't seem to find on my keyboard is the correct spacing.
-        full = []
-        for by_dash in div.text.strip().split('―'):
-            full.extend(by_dash.split(","))
-        quote = {
-            "quote": full[0],
-            "authorOrTitle": full[-min(len(full)-1, 2):]
-        }
+        # for some reason this weird dash which I can't seem to find on my keyboard is the correct spacing. Ok update I found that it is called the em dash. Also this is not the first time I am hearing about this, I have a sense of deja vu about this.
+        quoteText = div.text.strip().split('―')[0].strip()
+        quoteText = quoteText[1:-1]
+        # unable to put the ― em dash here as it's not going to (probably) the json correctly. Using hyphen instead
+        quoteText = quoteText.replace("\u2014", "-")
 
-        break  # here for checking, remove later
+        # get text from the spans
+        spans = div.find_all("span")
+        spanText = []
+        for span in spans:
+            spanText.append(span.text)
+        author = spanText[0]
+        title = "" if len(spanText) < 2 else spanText[-1].strip()
+        quote = {
+            "quote": quoteText,
+            "author": spanText[0].strip().replace(",", ""),
+            "title": title
+        }
+        quotes.append(quote)
+        x += 1
+        if x > 2:
+            break  # here to limit while checking, remove later
     break  # here for now while I just check, remove later
+
+# Clear the json file first
+with open("quotes.json", "w") as outfile:
+    json.dump(quotes, outfile)
