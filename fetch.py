925f1700550b8a52839e9acf323969b656601888
Arush Sharma <arush24astar@gmail.com>
2023-04-28 06:38:25 +0530

    remove training wheels

--- fetch.py
+++ fetch.py
@@ -1,12 +1,12 @@
-import json  # to make this into a json file
+import json
 import requests
 from bs4 import BeautifulSoup as bs4
 
+# We will store all the quotes in a list of dictionaries type of format
 quotes = []
 
-x = 1
-fresh = True
 for page in range(1, 30):
+    # Get data from the webpage
     url = "https://www.goodreads.com/author/quotes/5237.Hunter_S_Thompson?page=" + \
         str(page)
     response = requests.get(url)
@@ -18,6 +18,7 @@ for page in range(1, 30):
     for div in quote_divs:
         # for some reason this weird dash which I can't seem to find on my keyboard is the correct spacing. Ok update I found that it is called the em dash. Also this is not the first time I am hearing about this, I have a sense of deja vu about this.
         quoteText = div.text.strip().split('―')[0].strip()
+        # removing the double quotes as they were looking bad
         quoteText = quoteText[1:-1]
         # unable to put the ― em dash here as it's not going to (probably) the json correctly. Using hyphen instead
         quoteText = quoteText.replace("\u2014", "-")
@@ -35,11 +36,6 @@ for page in range(1, 30):
             "title": title
         }
         quotes.append(quote)
-        x += 1
-        if x > 2:
-            break  # here to limit while checking, remove later
-    break  # here for now while I just check, remove later
 
-# Clear the json file first
 with open("quotes.json", "w") as outfile:
     json.dump(quotes, outfile)
